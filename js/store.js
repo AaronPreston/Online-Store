5c759ce3a0b0d6c57d617ebbaa5fcefe4058f156
Aaron Preston <aaronpreston115@gmail.com>
2016-03-14 01:10:45 -0400

    Add menu

--- js/store.js
+++ js/store.js
@@ -1,30 +1,111 @@
-var _lets = document.getElementById('lets'),
-          lets_inf = document.getElementById('lets_info'),
-    _get = document.getElementById('get'),
-    _this = document.getElementById('this');
+var banner_item = document.getElementById('banner_item'),
+    banner_item_rng = 0,
+    menu_button = document.getElementById('menu_button'),
+    menu_b_press = 0,
+    menu = document.getElementById('menu');
 
-_lets.onmouseover = function() {
-  _lets.src = 'assets/lets.png';
-  lets_inf.style.marginTop = 0;
+
+var banner_item_loop = setInterval(function() {
+
+  banner_item.style.marginTop = '-500px';
+
+  setTimeout(function() {
+    banner_item_rng = Math.floor(Math.random() * 21) + 1;
+
+    switch(banner_item_rng) {
+      case 1:
+        banner_item.innerHTML = 'A Watch';
+        break;
+      case 2:
+        banner_item.innerHTML = 'A Phone';
+        break;
+      case 3:
+        banner_item.innerHTML = 'A TV';
+        break;
+      case 4:
+        banner_item.innerHTML = 'A Tablet';
+        break;
+      case 5:
+        banner_item.innerHTML = 'A Chair';
+        break;
+      case 6:
+        banner_item.innerHTML = 'A Desk';
+        break;
+      case 7:
+        banner_item.innerHTML = 'A Pillow';
+        break;
+      case 8:
+        banner_item.innerHTML = 'A Video Game';
+        break;
+      case 9:
+        banner_item.innerHTML = 'A Coffee Maker';
+        break;
+      case 10:
+        banner_item.innerHTML = 'A Fish Tank';
+        break;
+      case 11:
+        banner_item.innerHTML = 'A Coffee Mug';
+        break;
+      case 12:
+        banner_item.innerHTML = 'A T-Shirt';
+        break;
+      case 13:
+        banner_item.innerHTML = 'An Airsoft Gun';
+        break;
+      case 14:
+        banner_item.innerHTML = 'A Flash Drive';
+        break;
+      case 15:
+        banner_item.innerHTML = 'A DVD';
+        break;
+      case 16:
+        banner_item.innerHTML = 'A Computer Mouse';
+        break;
+      case 17:
+        banner_item.innerHTML = 'A Keyboard';
+        break;
+      case 18:
+        banner_item.innerHTML = 'A Microwave';
+        break;
+      case 19:
+        banner_item.innerHTML = 'A Laptop';
+        break;
+      case 20:
+        banner_item.innerHTML = 'A Game Console';
+        break;
+      case 21:
+        banner_item.innerHTML = 'A Fishing Pole';
+        break;
+      default:
+        banner_item.innerHTML = 'This.com';
+    }
+
+    banner_item.style.marginTop = '1px';
+  }, 1000);
+
+}, 5000);
+
+menu_button.onmousedown = function() {
+  menu_b_press += 1;
+  if(menu_b_press % 2 === 1) {
+    menu_button.src = 'assets/menu_button_down.png';
+
+    openMenu();
+
+  } else if(menu_b_press % 2 === 0) {
+    menu_button.src = 'assets/menu_button.png';
+
+    closeMenu();
+
+  }
 }
 
-_lets.onmouseout = function() {
-  _lets.src = 'assets/lets_hov.png';
-  lets_inf.style.marginTop = '-500px';
+openMenu = function() {
+  menu.style.right = '-2px';
+  menu_button.style.right = '36%';
 }
 
-_get.onmouseover = function() {
-  _get.src = 'assets/get.png';
-}
-
-_get.onmouseout = function() {
-  _get.src = 'assets/get_hov.png';
-}
-
-_this.onmouseover = function() {
-  _this.src = 'assets/this.png';
-}
-
-_this.onmouseout = function() {
-  _this.src = 'assets/this_hov.png';
+closeMenu = function() {
+  menu.style.right = '-36%';
+  menu_button.style.right = '12px';
 }
